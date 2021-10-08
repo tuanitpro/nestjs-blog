0446cc3527e85e44a306c3fbf18739229d9a8deb
Tuan Le Thanh <tuanitpro@gmail.com>
2021-10-08 17:00:03 +0700

    Implement swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthGuard } from './guards/auth.guard';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as compression from 'compression';
 
 async function bootstrap() {
@@ -12,6 +13,16 @@ async function bootstrap() {
   app.useGlobalGuards(new AuthGuard());
   app.useGlobalPipes(new ValidationPipe());
   app.use(new compression());
+  // app.setGlobalPrefix('api');
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Blog')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
